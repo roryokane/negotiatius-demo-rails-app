0b0e80c289e6d61e5a1affb5459d83cce598f5cf
Rory O’Kane <rory@roryokane.com>
2016-05-25 23:46:42 -0400

    Display the cart item count in the navigation bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def nav_cart_link_text
+    cart_items = current_user.cart_items
+    if cart_items.any?
+      "Cart (#{cart_items.size})"
+    else
+      'Cart'
+    end
+  end
 end
