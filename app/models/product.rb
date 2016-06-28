4c3cd53649edb3759729a75705f766b4b1b3a2f1
Rory O’Kane <rory@roryokane.com>
2016-06-28 11:47:35 -0400

    Fix NameError in production for ProductIdentifierPresenceValidator
    Apparently Rails does lazy loading with ActiveModel::Validations. The way it’s implemented, you can’t just include the validator you want – you have to `include` the whole module first, then use the validator class. The example in http://guides.rubyonrails.org/active_record_validations.html#performing-custom-validations shows the correct format, but doesn’t make it clear that that format can’t be deviated from.
    
    /Users/roryokane/Documents/Code/Rails/negotiatus-lite-demo/app/models/product.rb:5:in `<class:Product>': uninitialized constant ActiveModel::Validations::ProductIdentifierPresenceValidator (NameError)

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,8 @@ class Product < ActiveRecord::Base
   belongs_to :user
   has_many :cart_items, dependent: :destroy
 
-  validates_with ActiveModel::Validations::ProductIdentifierPresenceValidator
+  include ActiveModel::Validations
+  validates_with ProductIdentifierPresenceValidator
   validates :description, length: { maximum: 200 }
   validates :current_paying_price, numericality: { greater_than: 0, less_than: 1_000_000.00 }
   validates :total_amount_ordered, numericality: { greater_than_or_equal_to: 0 }
